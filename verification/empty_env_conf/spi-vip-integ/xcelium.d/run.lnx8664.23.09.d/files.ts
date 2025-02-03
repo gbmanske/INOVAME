06f540220496dc41b42b51b830911dd69f2c1f40
gbmanske <guilhermebarbosamanske@hotmail.com>
2025-02-03 20:10:49 -0300

    Delete verification/empty_env_conf/spi-vip-integ/xcelium.d directory

--- verification/empty_env_conf/spi-vip-integ/xcelium.d/run.lnx8664.23.09.d/files.ts
+++
@@ -1,3 +0,0 @@
-1738093253 /home/gme/guilherme.manske/INOVAME/verification/empty_env_conf/spi-vip-integ/class_a.sv
-1738093840 /home/gme/guilherme.manske/INOVAME/verification/empty_env_conf/spi-vip-integ/class_b.sv
-1738093728 /home/gme/guilherme.manske/INOVAME/verification/empty_env_conf/spi-vip-integ/top_tb.sv
